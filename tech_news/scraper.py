199ae16deb21f47f02bcf03d44d1fca2ebdcf4a0
willianmenezess <willmenezes@outlook.com.br>
2023-11-09 19:58:04 -0300

    feat: resolve requisito 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,15 +1,17 @@
 import requests
 import time
-# from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
+from typing import List
 
 
 # Requisito 1
 def fetch(url: str):
-    """Seu código deve vir aqui"""
+    """Recebe url e retorna o conteúdo HTML correspondente"""
     # respeita um rate limit de 1 segundo entre as requisições
     time.sleep(1)
     try:
         # requisição abandonada após 3 segundos sem resposta
+        # foi passado um header para evitar o erro 403
         response = requests.get(
             url, timeout=3, headers={"user-agent": "Fake user-agent"})
         if response.status_code == 200:
@@ -21,9 +23,16 @@ def fetch(url: str):
 
 
 # Requisito 2
-def scrape_updates(html_content):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+def scrape_updates(html_content: str) -> List[str]:
+    """retorna uma lista com as URLs das notícias"""
+    bs = BeautifulSoup(html_content, "html.parser")
+
+    news_divs = bs.find_all("div", {"class": "entry-thumbnail"})
+    result = []
+    for new in news_divs:
+        new = new.find("a")["href"]
+        result.append(new)
+    return result
 
 
 # Requisito 3
@@ -45,6 +54,6 @@ def get_tech_news(amount):
 
 
 if __name__ == "__main__":
-    url = "https://www.tecmundo.com.br/novidades"
+    url = "https://blog.betrybe.com"
     html = fetch(url)
-    print(html)
+    print(scrape_updates(html))
